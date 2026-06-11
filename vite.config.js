3881cf0310a69591d021c0b6077b2d79a4935e6e
Sam Manzano <sam.mnzn0@gmail.com>
2026-06-11 17:26:54 +0800

    chore: added react as a plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
