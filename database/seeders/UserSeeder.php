da95cacbb6f21d3669074e141878339322fc7fd4
Sam Manzano <sam.mnzn0@gmail.com>
2026-06-13 16:25:43 +0800

    feat: added UserSeeder for users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = [
+            [
+                'name' => 'Sam Manzano',
+                'email' => 'manzano.sheramae@ue.edu.ph',
+                'password' => '1q2w3r4r',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'Autumn Tandog',
+                'email' => 'tandog.autumnphenelope@ue.edu.ph',
+                'password' => 'pass1234',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'Charles Bryant Librada',
+                'email' => 'librada.charlesbryant@ue.edu.ph',
+                'password' => 'hello1234',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'Earl Joshua Espinosa',
+                'email' => 'espinosa.earljoshua@ue.edu.ph',
+                'password' => 'password',
+                'status' => 'active'
+            ]
+        ];
+
+        foreach($users as $userData) {
+            $user = User::create([
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'password' => $userData['password'],
+                'status' => 'active'
+            ]);
+
+            $user->role = $userData['role'];
+            $user->save();
+        }
+    }
+}
