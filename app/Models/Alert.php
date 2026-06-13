6b4bfb9440e8a8b15ad546789b41505b43f94594
Sam Manzano <sam.mnzn0@gmail.com>
2026-06-13 12:23:37 +0800

    feat: created Alert model

---
+++ app/Models/Alert.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Device;
+
+class Alert extends Model
+{
+    protected $fillable = [
+        'device_id',
+        'alert_type',
+        'alert_level',
+        'message',
+        'triggered_at',
+        'acknowledged_at',
+        'acknowledged_by',
+        'resolved_at',
+        'resolved_by',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'triggered_at' => 'datetime',
+        'acknowledged_at' => 'datetime',
+        'resolved_at' => 'datetime',
+        'is_active' => 'boolean'
+    ];
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
