01d4865009808c8560baeae1840430284cfe0e2b
Sam Manzano <sam.mnzn0@gmail.com>
2026-06-16 09:27:31 +0800

    fix: added fallback route in web.php to address 404 error in loading / -> /map

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// load app.blade.php
+Route::view('/', 'app');
+
+/**
+ * React fallback route. Any URL such as:
+ * /map, /overview, /devices/1, etc
+ * will open/load app.blade.php. 
+ * 
+ * Changed ->where('any', '.*')
+ * to: ->where('any', '^(?!api).*$')
+ * to ensure that API routes return JSON
+ * instead of React pages.
+ */
+Route::get('/{any}', function () {
     return view('app');
-});
+})->where('any', '^(?!api).*$');
